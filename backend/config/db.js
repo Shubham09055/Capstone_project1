224979836e1800c44753dbeff1ff70e3c986f106
Shubham09055 <shubham.garg22@st.niituniversity.in>
2025-04-30 16:17:18 +0530

    fr

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://mongo:27017/spamDetector", {
+    await mongoose.connect("mongodb://localhost:27017/spamDetector", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
